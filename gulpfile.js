2608873d5c6cd5197ca52395ec16694aa44af946
Caio Ladislau <caiolaszlo@gmail.com>
2017-07-06 11:01:34 -0300

    Submissão inicial

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
+
+// Set the banner content
+var banner = ['/*!\n',
+    ' * LibreClass CE Landingpage - v<%= pkg.version %>\n',
+    ' * Licensed under <%= pkg.license %>\n',
+    ' */\n',
+    ''
+].join('');
+
+
+// Minify compiled CSS
+gulp.task('minify-css', function() {
+    return gulp.src('css/main.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('js/main.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function() {
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'));
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'));
+
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('vendor/font-awesome'));
+})
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        },
+    });
+});
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'minify-css', 'minify-js', 'copy'], function() {
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('css/**/*.css', browserSync.reload);
+    gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('js/**/*.js', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+});
+
+// gulp.task('default', ['minify-c'dev'ss', 'minify-js', 'copy']);
+gulp.task('default', ['dev']);
